bc79b75ab0fef4da6b482d388af85ebf440a44cb
maaz7654 <i222125@nu.edu.pk>
2024-11-12 17:16:01 +0500

    fix

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GetInTouch = () => {
 	return (
-		<div className="text-center bg-white flex flex-col gap-4 py-20 justify-center bg-[#EFF6FF]">
+		<div className="text-center flex flex-col gap-4 py-20 justify-center bg-[#EFF6FF]">
 			<h1 className="text-5xl font-bold">Get in Touch</h1>
 			<p className="text-2xl text-gray-400">
 				Have questions or want to learn more? Contact us Today!
